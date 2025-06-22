0c83db608f16132c3b3a0ffdf963d38d10f3a74a
vervcv-v <cvverv@gmail.com>
2025-06-22 15:21:09 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://vervcv-v.github.io',
+    site: 'https://vervcv-v/vervcv-v.github.io/',
     integrations: [mdx(), react(), tailwind({
         applyBaseStyles: false,
     }), icon(), sitemap()],
