dff3ba9ba8582cb208d4bac85757fa6830843d7c
vervcv-v <cvverv@gmail.com>
2025-06-22 15:32:20 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,55 @@
-// @ts-check
-import {defineConfig} from 'astro/config';
 import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
-
-import tailwind from "@astrojs/tailwind";
-
-import icon from "astro-icon";
-
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import AutoImport from "astro-auto-import";
+import { defineConfig } from "astro/config";
+import remarkCollapse from "remark-collapse";
+import remarkToc from "remark-toc";
+import config from "./src/config/config.json";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://vervcv-v/vervcv-v.github.io/',
-    integrations: [mdx(), react(), tailwind({
-        applyBaseStyles: false,
-    }), icon(), sitemap()],
-    markdown: {
-        shikiConfig: {
-            theme: 'plastic',
-            wrap: true,
+  site: config.site.base_url ? config.site.base_url : "https://vervcv-cv.github.io",  // Asegúrate de colocar tu dominio aquí
+  base: config.site.base_path ? config.site.base_path : "/",
+  trailingSlash: config.site.trailing_slash ? "always" : "never",
+  output: "server",  // Salida para el servidor
+
+  vite: { plugins: [tailwindcss()] },
+  integrations: [
+    react(),
+    sitemap(),
+    AutoImport({
+      imports: [
+        "@/shortcodes/Button",
+        "@/shortcodes/Accordion",
+        "@/shortcodes/Notice",
+        "@/shortcodes/Video",
+        "@/shortcodes/Youtube",
+        "@/shortcodes/Tabs",
+        "@/shortcodes/Tab",
+      ],
+    }),
+    mdx(),
+  ],
+
+  markdown: {
+    remarkPlugins: [
+      remarkToc,
+      [
+        remarkCollapse,
+        {
+          test: "Table of contents",
         },
+      ],
+    ],
+    shikiConfig: {
+      theme: "one-dark-pro",  // Tema de syntax highlighting
+      wrap: true,
     },
-    experimental: {
-        svg: true,
-    }});
+    extendDefaultPlugins: true,
+  },
+
+  adapter: vercel(),  // Si estás usando Vercel, también puedes añadir el adaptador de Vercel
+});
